fde27cf483c8b194d70f7309e53695f98132eba9
askl56 <amscott@openmailbox.org>
2015-11-25 16:32:39 +0000

    Webpack config done

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,53 @@
 var path = require('path');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
+var merge = require('webpack-merge');
 
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
-
-module.exports = {
+var common = {
   entry: PATHS.app,
-  output: {
-    path: PATHS.build,
-    filename: 'bundle.js'
-  },
-
   module: {
     loaders: [
-    {
-      test: /\.css$/,
-      loaders: ['style', 'css'],
-      include: PATHS.app
-    }
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: PATHS.app
+      }
     ]
   },
-  devServer: {
-    historyApiFallback: true,
-    hot: true,
-    inline: true,
-    progress: true,
-
-    // display only errors to reduce the amount of output
-    stats: 'errors-only',
-
-    // parse host and port from env so this is easy
-    // to customize
-    host: process.env.HOST,
-    port: process.env.PORT
-  },
   plugins: [
-  new webpack.HotModuleReplacementPlugin(),
-  new HtmlwebpackPlugin({
-    title: 'Kanban app'
-  })
+    // important! move HotModuleReplacementPlugin below
+    //new webpack.HotModuleReplacementPlugin(),
+    new HtmlwebpackPlugin({
+      title: 'Kanban app'
+    })
   ]
-};
+  };
+
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+
+      // display only errors to reduce the amount of output
+      stats: 'errors-only',
+
+      // parse host and port from env so this is easy
+      // to customize
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
