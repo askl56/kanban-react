aafb0c179aa4815d16a12b9fa24db8069252696a
askl56 <amscott@openmailbox.org>
2015-11-25 19:24:16 +0000

    Finished implementing flux

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,48 +1,31 @@
+import AltContainer from 'alt-container';
 import React from 'react';
 import Notes from './Notes.jsx';
 import NoteActions from '../actions/NoteActions';
 import NoteStore from '../stores/NoteStore';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = NoteStore.getState();
-  }
-
-  componentDidMount() {
-    NoteStore.listen(this.storeChanged);
-  }
-
-  componentWillUnmount() {
-   NoteStore.unlisten(this.storeChanged);
- }
-
- storeChanged = (state) => {
-    // Without a property initializer `this` wouldn't
-    // point at the right context (defaults to `window`).
-    this.setState(state);
-  }
-
   render() {
-    const notes = this.state.notes;
     return (
       <div>
-      <button className="add-note" onClick={this.addNote}>+</button>
-      <Notes items={notes}
-      onEdit={this.editNote} onDelete={this.deleteNote} />
+        <button className="add-note" onClick={this.addNote}>+</button>
+        <AltContainer
+          stores={[NoteStore]}
+          inject={{
+            items: () => NoteStore.getState().notes
+          }}
+        >
+          <Notes onEdit={this.editNote} onDelete={this.deleteNote} />
+        </AltContainer>
       </div>
-      );
+    );
   }
-
   addNote() {
     NoteActions.create({task: 'New task'});
   }
-
   editNote(id, task) {
     NoteActions.update({id, task});
   }
-
   deleteNote(id) {
     NoteActions.delete(id);
   }
--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,9 @@
 import './main.css';
 import 'array.prototype.findindex';
 
+import alt from './libs/alt';
+import storage from './libs/storage';
+import persist from './libs/persist';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
@@ -8,6 +11,8 @@ import App from './components/App.jsx';
 main();
 
 function main() {
+  persist(alt, storage, 'app');
+
   const app = document.createElement('div');
 
   document.body.appendChild(app);
